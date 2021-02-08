e603ee1bce7a8a45322e8f211e5542f598d20b4a
David Mendonça <davidmed66@gmail.com>
2021-02-08 18:45:04 -0300

    Criação do componente de checkbox

---
+++ escola-amorinha/src/components/switch/CheckBox.js
@@ -0,0 +1,14 @@
+import { Checkbox, FormControl } from "@material-ui/core";
+import React from "react";
+
+const CheckBox = () => {
+  return (
+    <>
+      <FormControl>
+
+      </FormControl>
+    </>
+  );
+};
+
+export default CheckBox;
