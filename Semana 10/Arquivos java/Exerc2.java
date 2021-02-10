c429fb587dbcb51e69776d613a8374a4b6b7fcc9
David Mendonça <davidmed66@gmail.com>
2021-02-10 13:06:52 -0300

    exerc 2

---
+++ Semana 10/Arquivos java/Exerc2.java
@@ -0,0 +1,8 @@
+//Crie um programa Java que, ao executar, escreva na tela: Olá, Mundo!
+
+public class exerc2 {
+    public static void main(String[] args) {
+        String frase = "Olá Mundo";
+        System.out.print(frase);
+    }
+}
