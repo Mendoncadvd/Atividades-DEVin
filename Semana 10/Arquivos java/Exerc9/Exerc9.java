74ed0f2362508801ab1d86431c78ba1dddb3beb9
David Mendonça <davidmed66@gmail.com>
2021-02-11 17:17:31 -0300

    exerc 9

--- Semana 10/Arquivos java/Exerc9/Exerc9.java
+++ Semana 10/Arquivos java/Exerc9/Exerc9.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
+
 public class Exerc9 {
-    /* Crie um programa em Java que, utilizando a classe Scanner, peça ao usuário que digite um nome 
-    qualquer, e em seguida escreva na tela o tamanho de letras que o nome possui. */
+    /*
+     * Crie um programa em Java que, utilizando a classe Scanner, peça ao usuário
+     * que digite um nome qualquer, e em seguida escreva na tela o tamanho de letras
+     * que o nome possui.
+     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome de uma cidade");
-        String cidade = sc.nextLine();
-        System.out.print("A cidade tem " + cidade.length() + " letras");
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * System.out.println("Digite o nome de uma cidade"); String cidade =
+         * sc.nextLine(); System.out.print("A cidade tem " + cidade.length() +
+         * " letras");
+         */
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
     }
 }
