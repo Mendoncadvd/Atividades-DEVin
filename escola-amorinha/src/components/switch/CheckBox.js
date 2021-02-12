ce947970cb7a6b3f57f87988ff89d52d5d870559
David Mendonça <davidmed66@gmail.com>
2021-02-12 13:38:55 -0300

    alterações do componente de checkbox

--- escola-amorinha/src/components/switch/CheckBox.js
+++ escola-amorinha/src/components/switch/CheckBox.js
@@ -1,12 +1,20 @@
-import { Checkbox, FormControl } from "@material-ui/core";
+import { Checkbox, FormControl, FormLabel, FormGroup, FormControlLabel } from "@material-ui/core";
 import React from "react";
 
 const CheckBox = () => {
   return (
     <>
-      <FormControl>
-        
-      </FormControl>
+    {/* Os componente abaixo não funcionam. Tentar outra forma */}
+      {/* <FormControl>
+        <FormLabel>Selecione a turma</FormLabel>
+        <FormGroup column>
+          <FormControlLabel 
+          label="Turma11"
+          labelPlacement="end"
+          control={<CheckBox />}
+          />
+        </FormGroup>
+      </FormControl>  */}
     </>
   );
 };
